738dc6b10b3fc0eb705c53ccaab2fa70e912274d
James Bunn <madhatter84gn@fastmail.com>
2025-10-11 19:03:02 -0400

    completed challenge

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
-def steps(number):
-    pass
+def steps(number: int) -> int:
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+    step_count = 0
+    while number != 1:
+        step_count += 1
+        if number % 2 == 0:
+            number = int(number / 2)
+        else:
+            print("ODD: ", number)
+            number = (number * 3) + 1
+    return step_count
