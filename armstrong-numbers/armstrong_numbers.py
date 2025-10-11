3c09eb283a286893399b9af6620078e694f059c4
James Bunn <madhatter84gn@fastmail.com>
2025-10-11 18:47:12 -0400

    completed challenge

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,9 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(number: int) -> bool:
+    digits = [int(digit) for digit in str(number)]
+    number_of_digits = len(digits)
+
+    total = 0
+    for d in digits:
+        total += d**number_of_digits
+
+    return total == number
