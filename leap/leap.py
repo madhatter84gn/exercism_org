295d6695616b515834a6404ac986105b94754f3c
James Bunn <madhatter84gn@fastmail.com>
2025-10-12 08:16:16 -0400

    Completed the challenge

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,8 @@
-def leap_year(year):
-    pass
+def leap_year(year) -> bool:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+        else:
+            return True
+    return False
