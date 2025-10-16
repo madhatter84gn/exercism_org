b2777b7cab71dbbd5c52f1dce4d28727de3b16f7
James Bunn <madhatter84gn@fastmail.com>
2025-10-16 18:03:33 -0400

    complete raindrops challenge

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,19 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+    results = []
+    is_div_by_three = number % 3 == 0
+    is_div_by_five = number % 5 == 0
+    is_div_by_seven = number % 7 == 0
+
+    if is_div_by_three:
+        results.append("Pling")
+
+    if is_div_by_five:
+        results.append("Plang")
+
+    if is_div_by_seven:
+        results.append("Plong")
+
+    if len(results) < 1:
+        return str(number)
+
+    return "".join(results)
