337880f7c77ab707481ed73664a8e2cfd6e1e3b3
James <madhatter84gn@fastmail.com>
2025-10-16 13:58:12 -0400

    complete bob challenge

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,30 @@
-def response(hey_bob):
-    pass
+def response(hey_bob: str) -> str:
+    is_question = is_input_a_question(hey_bob)
+    is_shouting = is_input_shouting(hey_bob)
+    is_silent_or_whitespace = is_input_silent(hey_bob)
+
+    if is_shouting and is_question:
+        return "Calm down, I know what I'm doing!"
+
+    if is_question:
+        return "Sure."
+
+    if is_shouting:
+        return "Whoa, chill out!"
+
+    if is_silent_or_whitespace:
+        return "Fine. Be that way!"
+
+    return "Whatever."
+
+
+def is_input_silent(input: str) -> bool:
+    return not input.strip()
+
+
+def is_input_shouting(input: str) -> bool:
+    return input.isupper()
+
+
+def is_input_a_question(input: str) -> bool:
+    return input.strip().endswith("?")
